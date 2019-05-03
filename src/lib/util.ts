e7acbe386979cd0ab18754ab803670237231110a
Jingkun Hua <huajingkun1214@gmail.com>
2019-05-03 16:44:08 +0800

    feat(util): add IsUndefinedOrNever

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -27,19 +27,10 @@ export type ExcludeFromArray<T, U> = (
 
 export type IsAny<T, TrueType = true, FalseType = false> = 1 extends (T extends never ? 1 : 0) ? TrueType : FalseType
 export type IsInvariant<T, U, TrueType = true, FalseType = false> = [T] extends [U] ? ([U] extends [T] ? TrueType : FalseType) : FalseType
-export type IsInvariantWithoutAny<T, U, TrueType = true, FalseType = false> = IsAny<T, FalseType, IsAny<U, FalseType, IsInvariant<T, U, TrueType, FalseType>>>
 export type IsNever<T, TrueType = true, FalseType = false> = [T] extends [never] ? TrueType : FalseType
 
-/**
- * Check if a type is `true`.
- * @example
- * type A = IsTrue<any>; // false
- * type B = IsTrue<never>; // false
- * type C = IsTrue<unknown>; // error
- * type D = IsTrue<boolean>; // false
- * type E = IsTrue<false>; // false
- * type F = IsTrue<true>; // true
- */
+export type IsInvariantWithoutAny<T, U, TrueType = true, FalseType = false> = IsAny<T, FalseType, IsAny<U, FalseType, IsInvariant<T, U, TrueType, FalseType>>>
+export type IsUndefinedOrNever<T, TrueType = true, FalseType = false> = IsInvariant<T, undefined, TrueType, IsNever<T, TrueType, FalseType>>
 export type IsTrue<T extends boolean, TrueType = true, FalseType = false> = IsInvariantWithoutAny<T, true, TrueType, FalseType>
 
 export type FilterUndefinedKeys<T extends object> = { [Key in keyof T]: undefined extends T[Key] ? Key : never }[keyof T]
