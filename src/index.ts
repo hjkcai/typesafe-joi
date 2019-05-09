dd98b6d62a0f2cf7e8f437255e830874eff79c9f
hjkcai <huajingkun1214@gmail.com>
2019-05-09 15:46:53 +0800

    feat: export SchemaLike

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// Type definitions for joi 14.3
+// Type definitions for @hapi/joi 15.x
 // Project: https://github.com/hapijs/joi
 // Definitions by: Jingkun Hua <https://github.com/hjkcai>
-// TypeScript Version: 3.0
+// TypeScript Version: >=3.0
 // @ts-ignore
 /// <reference types="node" />
 /// <reference lib="es2015" />
@@ -11,5 +11,7 @@ export * from './lib/joi'
 export * from './lib/schema'
 export * from './lib/value'
 export * from './schema'
+
+export type SchemaLike = import('./lib/schema').Schema.SchemaLike
 
 module.exports = require('joi')
