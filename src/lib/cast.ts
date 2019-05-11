cc3fa54bc49888988a6555bceccc90a54e471d53
Jingkun Hua <huajingkun1214@gmail.com>
2019-05-11 10:35:00 +0800

    feat(cast): type cast defaults to any

--- src/lib/cast.ts
+++ src/lib/cast.ts
@@ -4,34 +4,34 @@ import * as Schemas from '../schema'
 type CastValue<T> = Value<T, never, never, never, true>
 
 /** Cast the value type of a AlternativesSchema. */
-export type Alternatives<T> = Schemas.AlternativesSchema<CastValue<T>>
+export type Alternatives<T = any> = Schemas.AlternativesSchema<CastValue<T>>
 
 /** Cast the value type of a AnySchema. */
-export type Any<T> = Schemas.AnySchema<CastValue<T>>
+export type Any<T = any> = Schemas.AnySchema<CastValue<T>>
 
 /** Cast the value type of a ArraySchema. */
-export type Array<T> = Schemas.ArraySchema<CastValue<T>>
+export type Array<T = any> = Schemas.ArraySchema<CastValue<T>>
 
 /** Cast the value type of a BinarySchema. */
-export type Binary<T> = Schemas.BinarySchema<CastValue<T>>
+export type Binary<T = any> = Schemas.BinarySchema<CastValue<T>>
 
 /** Cast the value type of a BooleanSchema. */
-export type Boolean<T> = Schemas.BooleanSchema<CastValue<T>>
+export type Boolean<T = any> = Schemas.BooleanSchema<CastValue<T>>
 
 /** Cast the value type of a DateSchema. */
-export type Date<T> = Schemas.DateSchema<CastValue<T>>
+export type Date<T = any> = Schemas.DateSchema<CastValue<T>>
 
 /** Cast the value type of a FunctionSchema. */
-export type Function<T> = Schemas.FunctionSchema<CastValue<T>>
+export type Function<T = any> = Schemas.FunctionSchema<CastValue<T>>
 
 /** Cast the value type of a LazySchema. */
-export type Lazy<T> = Schemas.LazySchema<CastValue<T>>
+export type Lazy<T = any> = Schemas.LazySchema<CastValue<T>>
 
 /** Cast the value type of a NumberSchema. */
-export type Number<T> = Schemas.NumberSchema<CastValue<T>>
+export type Number<T = any> = Schemas.NumberSchema<CastValue<T>>
 
 /** Cast the value type of a ObjectSchema. */
-export type Object<T> = Schemas.ObjectSchema<CastValue<T>>
+export type Object<T = any> = Schemas.ObjectSchema<CastValue<T>>
 
 /** Cast the value type of a StringSchema. */
-export type String<T> = Schemas.StringSchema<CastValue<T>>
+export type String<T = any> = Schemas.StringSchema<CastValue<T>>
